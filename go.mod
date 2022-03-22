91639681a61ea88d1b57efa49bb771beee73c9b3
akhil sharma <akhil.sharma@myrl.tech>
2022-03-22 12:12:02 +0530

    complete working code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/akhil/slack-file-bot
+
+go 1.16
+
+require github.com/slack-go/slack v0.9.1 // indirect
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-310246501329-2246808119766-C2TQU9hIq6qMt5vloSXVSkQ7")
+	os.Setenv("CHANNEL_ID", "C9478EZ7B")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"ZIPL.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+	}
+}
